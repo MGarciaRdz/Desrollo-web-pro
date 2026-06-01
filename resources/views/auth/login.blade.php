c7652323161e2098421fa1e85d9ddcf654b48c93
Mario Garcia <MGarcia220904@hotmail.com>
2026-06-01 15:05:27 -0600

    feat: Add reCAPTCHA support to login form and update services configuration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,9 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -32,12 +35,16 @@
             </label>
         </div>
 
+        <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.site_key') }}"></div>
+
+
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
