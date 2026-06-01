a47269f193491fe49122cf190f0e56019658ac5e
Mario Garcia <MGarcia220904@hotmail.com>
2026-06-01 15:17:46 -0600

    feat: Integrate Google reCAPTCHA into registration and guest layout views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 <x-guest-layout>
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -38,6 +40,9 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
+
+    
+        <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.site_key') }}"></div>
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
